345541a614161f727837c0be138a73e6e3f8dd49
braican <nick.braica@gmail.com>
2014-09-15 10:55:05 -0400

    config sample

---
+++ js/config-sample.js
@@ -0,0 +1,11 @@
+
+(function(BEER){
+
+    // the untappd api secret
+    BEER.api_secret = 'API_SECRET';
+    
+    // the untappd api id
+    BEER.api_id = 'API_ID';
+    
+
+})(window.BEER = window.BEER || {});
