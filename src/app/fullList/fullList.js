c4e38375a320db9b3427a9ea30680a644c267f9c
Nick Braica <nick.braica@gmail.com>
2016-09-20 15:31:12 -0400

    angular fire

--- src/app/fullList/fullList.js
+++ src/app/fullList/fullList.js
@@ -3,8 +3,8 @@ angular.module('drinkingBuddyApp.fullList', ['ngRoute'])
 
 .config(['$routeProvider', function( $routeProvider ){
     $routeProvider.when('/full',{
-        template   : 'fullList.html',
-        controller : 'FullListCtrl'
+        templateUrl : 'fullList.html',
+        controller  : 'FullListCtrl'
     });
 }])
 
