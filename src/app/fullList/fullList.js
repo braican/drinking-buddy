b35eb24bb996ae099b60449aa9bf69d83bb297de
Nick Braica <nick.braica@gmail.com>
2016-12-29 18:09:17 -0500

    stuff:

--- src/app/fullList/fullList.js
+++ src/app/fullList/fullList.js
@@ -4,23 +4,49 @@ angular.module('drinkingBuddyApp.fullList', ['ngRoute'])
 .config(['$routeProvider', function( $routeProvider ){
     $routeProvider.when('/full',{
         templateUrl : 'fullList.html',
-        controller  : 'FullListCtrl'
+        controller  : 'FullListCtrl as beerlist'
     });
 }])
 
 .controller('FullListCtrl', [
     '$scope',
     'userData',
-    'userBeers',
-function( $scope, userData, userBeers ){
+function( $scope, userData ){
 
+    // cache
     var vm = this;
 
-    // vm.user = userData.data;
 
-    vm.beers = userBeers.getBeers();
+    vm.data = userData.data;
 
 
-    // console.log( vm.beers );
+
+    // userData.getCheckins().then(function( resp ){
+    //     console.log( resp );
+    // });
+
+
+    // untappd.getUserData( "rossflowers" ).then(function( resp ){
+    //     console.log( resp );
+    // });
+
+    // var
+    //     username    = userData.getUsername(),
+    //     beerPromise = userBeers.getBeers( username );
+
+    // // array of all the checkins
+    // vm.checkins;
+
+
+    // // get the user beers, if we have a valid promise
+    // if( beerPromise ){
+
+    //     beerPromise.then( function( checkins ){
+    //         vm.checkins = checkins;
+    //         console.log( checkins );
+    //     });
+    // }
+
+
 
 }]);
