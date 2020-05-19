4952d02eb5bf1d719dc36ece35a94dee3d7672be
Nick Braica <nick.braica@gmail.com>
2020-05-19 15:26:45 -0700

    Setting up the initial scripting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <ul>
+      <li v-for="checkin in checkins">
+        <pre>{{ checkin }}</pre>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script></script>
+<script>
+// import data from '../../data/BASE_braican.json';
+// console.log(data);
+
+export default {
+  name: 'Home',
+
+  data() {
+    return {
+      checkins: [],
+    };
+  },
+};
+</script>
