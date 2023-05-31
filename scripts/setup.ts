4c782cebf84aea441d8142fabcdb026967bc1bfe
Nick Braica <nick.braica@gmail.com>
2023-05-31 11:47:46 -0400

    Starting to set up

---
+++ scripts/setup.ts
@@ -0,0 +1,21 @@
+import { Tigris } from '@tigrisdata/core';
+import { TodoItem } from '../db/models/checkin';
+
+
+async function main() {
+  const tigrisClient = new Tigris();
+  await tigrisClient.getDatabase().initializeBranch();
+  await tigrisClient.registerSchemas([TodoItem]);
+
+}
+
+main()
+  .then(async () => {
+    // eslint-disable-next-line
+    console.log('Setup complete ...');
+    process.exit(0);
+  })
+  .catch(async e => {
+    console.error(e);
+    process.exit(1);
+  });
