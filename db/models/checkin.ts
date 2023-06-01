65803c2059880d717c90723c6fcff8f18725d9c7
Nick Braica <nick.braica@gmail.com>
2023-06-01 11:12:15 -0400

    Setting up schema

--- db/models/checkin.ts
+++ db/models/checkin.ts
@@ -8,15 +8,101 @@ import {
 
 import type { TigrisCollectionType } from '@tigrisdata/core';
 
+class Beer {
+  @Field()
+  id!: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  label: string;
+
+  @Field()
+  style: string;
+
+  @Field()
+  abv: number;
+}
+
+class Brewery {
+  @Field()
+  id!: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  type: string;
+
+  @Field()
+  label: string;
+
+  @Field()
+  city: string;
+
+  @Field()
+  state: string;
+
+  @Field()
+  country: string;
+
+  @Field()
+  lat: number;
+
+  @Field()
+  lng: number;
+}
+
+class Venue {
+  @Field()
+  id!: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  address: string;
+
+  @Field()
+  city: string;
+
+  @Field()
+  state: string;
+
+  @Field()
+  country: string;
+
+  @Field()
+  lat: number;
+
+  @Field()
+  lng: number;
+}
+
 @TigrisCollection('checkins')
 export class Checkin implements TigrisCollectionType {
   @PrimaryKey(TigrisDataTypes.INT32, { order: 1, autoGenerate: true })
   id!: number;
 
   @Field()
-  @SearchField()
-  text!: string;
+  checkinId!: string;
 
   @Field()
-  completed!: boolean;
+  createdAt!: Date;
+
+  @Field()
+  comment: string;
+
+  @Field()
+  rating: number;
+
+  @Field()
+  beer: Beer;
+
+  @Field()
+  brewery: Brewery;
+
+  @Field()
+  venue: Venue;
 }
