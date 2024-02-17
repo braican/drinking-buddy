374d2d366f13be25ac40f18f53e9b5ee835ab476
Nick Braica <nick.braica@gmail.com>
2024-02-16 17:09:20 -0800

    Upgrade to svelte 2

--- src/routes/brewery/[slug]/+page.ts
+++ src/routes/brewery/[slug]/+page.ts
@@ -8,7 +8,7 @@ export async function load({ fetch, params }) {
     const { brewery } = await req.get<{ brewery: Brewery }>(`brewery?slug=${params.slug}`);
 
     if (!brewery) {
-      throw error(404);
+      error(404);
     }
 
     const { beers } = await req.get<{ beers: Beer[] }>(`brewery/${brewery.id}/beers`);
@@ -22,7 +22,7 @@ export async function load({ fetch, params }) {
     };
   } catch (err) {
     if (err.status === 404) {
-      throw error(404, 'Brewery not found.');
+      error(404, 'Brewery not found.');
     }
 
     return {};
