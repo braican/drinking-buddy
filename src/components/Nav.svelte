98f051560d1759c26065acbdf57a455102424bb0
Nick Braica <nick.braica@gmail.com>
2026-04-08 22:47:54 -0400

    better menu

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,18 +1,12 @@
 <script>
   import { viewStore } from '@stores';
   import { Search } from '@components';
-  import { HomeIcon, FiltersIcon, SearchIcon } from '@icons';
+  import { SearchIcon } from '@icons';
 
   const { searchVisible } = viewStore;
 </script>
 
 <nav class="nav">
-  <a class="nav-button" href="/" aria-label="Home">
-    <HomeIcon />
-  </a>
-  <a class="nav-button" href="/filters" aria-label="Filters">
-    <FiltersIcon />
-  </a>
   <button class="nav-button" aria-label="Search" on:click={viewStore.showSearch}>
     <SearchIcon />
   </button>
@@ -25,10 +19,8 @@
 <style lang="scss">
   .nav {
     position: fixed;
-    gap: var(--spacing-sm);
     bottom: var(--spacing-base);
     right: var(--spacing-base);
-    display: flex;
     z-index: 99;
   }
 
